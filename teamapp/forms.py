301dcc65b675f324c433f1428fcf837486d97d88
s1f102102481 <s1f102102481@iniad.org>
2023-10-30 17:14:03 +0900

    Langchainとの連携
    データベースは参考サイトのsqlite-sakila.dbを使用
    今のところ連携はできたが、出力結果がsearch_db.htmlに反映されず、powershellにだけ表示されてしまうので、そこを修正していきたい

--- teamapp/forms.py
+++ teamapp/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class ChatForm(forms.Form):
 
     sentence = forms.CharField(label='チャット', widget=forms.Textarea(), required=True)
+    
+
+class SearchDBForm(forms.Form):
+    question = forms.CharField(label='質問', max_length=100)
--- teamapp/urls.py
+++ teamapp/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('result', views.result, name='viewresult'),
+    #path('search_db/', views.search_db, name='search_db'),
+    path('search_db/', views.search_db, name='search_db'),
+
 ]
