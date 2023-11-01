c208125a46c06266dba44752c7a02b304fc11c79
1f10210146 <s1f102101468@iniad.org>
2023-11-01 14:16:20 +0900

    ホームデザイン

--- teamapp/forms.py
+++ teamapp/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
 class ChatForm(forms.Form):
-
-    sentence = forms.CharField(label='チャット', widget=forms.Textarea(), required=True)
+    sentence = forms.CharField(label='チャット', widget=forms.Textarea(attrs={'placeholder' :"調べたい問題を入力してください","style":"height:20px"}), required=True)
